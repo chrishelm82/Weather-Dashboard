27dad3adccdacaf10736714de6a3cbfed9c20ea5
The Matrix <helm.chris82@gmai.com>
2019-12-16 22:34:06 -0500

    added container to html

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-$(function (){
+$(function () {
+            $.ajax({
+                type
+                url: "d2ccc27ebc1c88c39919cbfa59aa9fde"
+                method: "GET"
+            }).then(function (response) {
+                console.log("data")
 
-$.ajax({
-    type
-    url: "d2ccc27ebc1c88c39919cbfa59aa9fde"
-    method: "GET"
-}).then(function(response){
-    console.log("data")
-
-});
+            });
